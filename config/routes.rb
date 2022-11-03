9ea51b38a0a5b1495c25cc30572c3d892aa2edac
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-11-04 01:07:03 +0300

    Add React view

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1, defaults: { format: 'json' } do
-      resources :greetings
-    end
-  end
-
-  get '*page', to: 'greeting#index', constraints: lambda { |req|
-    !req.xhr? && req.format.html?
-  }
+  get 'greeting', to: 'greeting#index'
 
   # Defines the root path route ("/")
-  root "greeting#index"
+  root "root#index"
 end
