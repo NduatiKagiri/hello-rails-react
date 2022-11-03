9788731be4787597c650ccda4c27e98b17c9501d
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-11-03 23:47:30 +0300

    Create API endpoint

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,4 +1,6 @@
 class GreetingController < ApplicationController
   def index
+    @greeting = Greeting.order('RANDOM()').first
+    render json: @greeting
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :greetings
+    end
+  end
+
+  get '*page', to: 'greeting#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
 
   # Defines the root path route ("/")
-  root "greetings#index"
+  root "greeting#index"
 end
